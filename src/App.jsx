99f5a1d096410f06bb70fae4052047c5e6db66ea
William Alonso <williamalonso@outlook.com>
2023-08-14 23:42:34 -0300

    chaging App

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import store from './store';
 import Pages from './pages/Pages';
 import { Provider } from 'react-redux';
 import StyledGlobal from './components/StyledComponents/StyledGlobal';
+import Category from './components/Category';
 
 function App() {
   return (
     <Provider store={store}>
       <StyledGlobal />
+      <Category />
       <Pages />
     </Provider>
   );
