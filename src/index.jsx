c6da317326f1143c5493329257d02cbb5059ebca
William Alonso <williamalonso@outlook.com>
2023-08-19 00:53:38 -0300

    new component

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Routing from './Routes';
+import StyledGlobal from './components/StyledComponents/StyledGlobal';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <StyledGlobal />
     <Routing />
   </React.StrictMode>
 );
